49ce1155b162d1a167d72060566a818f1ebd7c7a
MrAyushGautam <info.ayushgautam@gmail.com>
2024-09-20 18:29:15 +0530

    Initial Commit

---
+++ Dr. UPSC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dr. UPSC
+//
+//  Created by Morning Star on 20/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
